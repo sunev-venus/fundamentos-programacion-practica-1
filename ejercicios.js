30a2b3d0970390deeea90ea3f8dce8e580373b68
venus semino <venus.semino@tecdesoftware.edu.mx>
2025-11-05 12:49:32 -0600

    ejercicio 1 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -15,9 +15,9 @@ function miInformacion() {
   // TODO: Declara las variables nombre, edad y carrera
   // Ejemplo: const nombre = "Juan";
   
-  const nombre = "";
-  const edad = 0;
-  const carrera = "";
+  const nombre = "Venus Getsemaní Semino Alemán";
+  const edad = 18;
+  const carrera = "TSU";
   
   return { nombre, edad, carrera };
 }
