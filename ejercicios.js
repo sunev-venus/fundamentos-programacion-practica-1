b6dce7958db586219fee57c9187159644e733ae3
sunev-venus <venus.semino@tecdesoftware.edu.mx>
2025-11-06 19:37:28 -0600

    Ejercicio 3.4 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -190,7 +190,12 @@ function tablaMultiplicar(numero) {
  */
 function numerosPares(n) {
   // TODO: Crea un array con todos los nÃºmeros pares hasta n
-  const pares = [];
+const pares = [];
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 === 0) {
+      pares.push(i);
+    }
+  }
   return pares;
 }
 
